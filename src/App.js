29f9bad506a1e791839895bf0b0ba1b5b28d7d9d
JoelMathewC <joelmathewcherian@gmail.com>
2021-08-27 23:04:09 +0400

    Bootstrap link complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <h1>ToDoList</h1>
+      <h2 className="display-1">Hello</h2>
+      <Button>Test Button</Button>
+    </div>
   );
 }
 
